d0a47dd201f7b8d57f5686e9ab5905f1e3b125cf
Shawkakessy <142739905+ShawkaKessy@users.noreply.github.com>
2024-11-17 14:15:06 +0300

    Add files via upload

---
+++ Randoms.java
@@ -0,0 +1,32 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    private final int min;
+    private final int max;
+    private final Random random;
+
+    public Randoms(int min, int max) {
+        if (min > max) {
+            throw new IllegalArgumentException("Минимальное значение не может быть больше максимального");
+        }
+        this.min = min;
+        this.max = max;
+        this.random = new Random();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true; // Бесконечная последовательность
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min + 1) + min; // Генерация случайного числа в диапазоне [min, max]
+            }
+        };
+    }
+}
